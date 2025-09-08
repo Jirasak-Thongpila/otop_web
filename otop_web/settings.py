1adcc1dbd60468dd05bd6b761d2dffc67e640746
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-08 10:01:02 +0700

    set app,templates

--- otop_web/settings.py
+++ otop_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'otop',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'otop_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': Path.joinpath[BASE_DIR, 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
