f3ff29cc01ff39317daef3ad56a35506880fcd22
Jirasak-Thongpire <tle0613828245@gmail.com>
2025-09-08 10:12:51 +0700

    set views,urls

--- otop_web/settings.py
+++ otop_web/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'otop_web.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': Path.joinpath[BASE_DIR, 'templates'],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
